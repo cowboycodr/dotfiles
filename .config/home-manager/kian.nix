ca90ba2a2b117299f363aa99a2311a868610f185
cowboycodr <kian.p.m.007@gmail.com>
2025-06-08 16:09:06 -0500

    kian.nix

---
+++ .config/home-manager/kian.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "kian";
+  home.homeDirectory = "/home/kian";
+
+  nixpkgs.config.allowUnfree = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  imports = [
+    ./packages/desktop.nix
+    ./packages/neovim.nix
+    ./packages/terminal.nix
+  ];
+}
